918e972aed0e79337567a8149af69fa807ea6f30
Kjell Wooding <kjell@wooding.org>
2018-07-05 16:04:28 -0400

    import core files to start a new base organization

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/data/make_dataset.py
@@ -3,17 +3,29 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
+from .datasets import fetch_and_unpack, available_datasets
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('project_dir', type=click.Path(exists=True))
+def main(project_dir, datasets=None):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
+
+    Raw files are downloaded into `project_dir`/data/raw
+    Interim files are generated in `project_dir`/data/interim
+
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    if datasets is None:
+        datasets = available_datasets
+
+    data_dir = Path(project_dir) / 'data'
+
+    unpacked_datasets = {}
+    for dataset_name in datasets:
+        unpacked_datasets[dataset_name] = fetch_and_unpack(dataset_name, data_dir=data_dir)
 
 
 if __name__ == '__main__':
---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/data/utils.py
@@ -0,0 +1,190 @@
+import hashlib
+import os
+import pathlib
+import tarfile
+import requests
+import logging
+import shutil
+import zipfile
+import gzip
+
+from ..paths import interim_data_path
+
+logger = logging.getLogger(__name__)
+
+hash_function = {
+    'sha1': hashlib.sha1,
+    'sha256': hashlib.sha256,
+    'md5': hashlib.md5,
+}
+
+
+def hash_file(fname, algorithm="sha1", block_size=4096):
+    '''Compute the hash of an on-disk file
+
+    algorithm: {'md5', sha1', 'sha256'}
+        hash algorithm to use
+    block_size:
+        size of chunks to read when hashing
+
+    Returns:
+        Hashlib object
+    '''
+    hashval = hash_function[algorithm]()
+    with open(fname, "rb") as fd:
+        for chunk in iter(lambda: fd.read(block_size), b""):
+            hashval.update(chunk)
+    return hashval
+
+def fetch_files(force=False, dst_dir=None, **kwargs):
+    '''
+    fetches a list of files via URL
+
+    url_list: list of dicts, each containing:
+        url:
+            url to be downloaded
+        hash_type:
+            Type of hash to compute
+        hash_value: (optional)
+            if specified, the hash of the downloaded file will be
+            checked against this value
+        name: (optional)
+            Name of this dataset component
+        raw_file:
+            output file name. If not specified, use the last
+            component of the URL
+    '''
+    url_list = kwargs.get('url_list', None)
+    if not url_list:
+        raise Exception(f"url_list is a required keyword argument: {kwargs}")
+    result_list = []
+    for url_dict in url_list:
+        name = url_dict.get('name', 'dataset')
+        logger.info(f"Fetching {name}")
+        result_list.append(fetch_file(force=force, dst_dir=dst_dir, **url_dict))
+    return all([r[0] for r in result_list]), result_list
+
+def fetch_file(url,
+               raw_file=None, dst_dir=None,
+               force=False,
+               hash_type="sha1", hash_value=None,
+               **kwargs):
+    '''Fetch remote files via URL
+
+    url:
+        url to be downloaded
+    hash_type:
+        Type of hash to compute
+    hash_value: (optional)
+        if specified, the hash of the downloaded file will be
+        checked against this value
+    name: (optional)
+        Name of this dataset component
+    raw_file:
+        output file name. If not specified, use the last
+        component of the URL
+    dst_dir:
+        directory to place downloaded files
+    force: boolean
+        normally, the URL is only downloaded if `raw_file` is
+        not present on the filesystem, or if the existing file has a
+        bad hash. If force is True, download is always attempted.
+
+
+    returns one of:
+
+
+        (HTTP_Code, downloaded_filename, hash) (if downloaded from URL)
+        (True, filename, hash) (if already exists)
+        (False, [error])
+    if `raw_file` already exists, compute the hash of the on-disk file,
+    '''
+    if dst_dir is None:
+        dst_dir = pathlib.Path(".")
+    if raw_file is None:
+        raw_file = url.split("/")[-1]
+    raw_data_path = pathlib.Path(dst_dir)
+
+    if not os.path.exists(raw_data_path):
+        os.makedirs(raw_data_path)
+
+    raw_data_file = raw_data_path / raw_file
+
+    if os.path.exists(raw_data_file):
+        raw_file_hash = hash_file(raw_data_file, algorithm=hash_type).hexdigest()
+        if hash_value is not None:
+            if raw_file_hash == hash_value:
+                if force is False:
+                    logger.info(f"{raw_file} exists and hash is valid")
+                    return True, raw_data_file, raw_file_hash
+            else:
+                logger.warning(f"{raw_file} exists but has bad hash {raw_file_hash}."
+                               " Re-downloading")
+        else:
+            if force is False:
+                logger.info(f"{raw_file} exists, but no hash to check")
+                return True, raw_data_file, raw_file_hash
+
+    # Download the file
+    try:
+        results = requests.get(url)
+        results.raise_for_status()
+        raw_file_hash = hash_function[hash_type](results.content).hexdigest()
+        if hash_value is not None:
+            if raw_file_hash != hash_value:
+                print(f"Invalid hash on downloaded {raw_file}"
+                      f" ({hash_type}:{raw_file_hash}) != {hash_type}:{hash_value}")
+                return False, None, raw_file_hash
+        logger.info(f"Writing {raw_data_file}")
+        with open(raw_data_file, "wb") as code:
+            code.write(results.content)
+    except requests.exceptions.HTTPError as err:
+        return False, err, None
+
+    return results.status_code, raw_data_file, raw_file_hash
+
+def unpack(filename, dst_dir=None, create_dst=True):
+    '''Unpack a compressed file
+
+    filename: path
+        file to unpack
+    dst_dir: path (default paths.interim_data_path)
+        destination directory for the unpack
+    create_dst: boolean
+        create the destination directory if needed
+    '''
+    if dst_dir is None:
+        dst_dir = interim_data_path
+
+    if create_dst:
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+
+    # in case it is a Path
+    path = str(filename)
+
+    archive = False
+    if path.endswith('.zip'):
+        archive = True
+        opener, mode = zipfile.ZipFile, 'r'
+    elif path.endswith('.tar.gz') or path.endswith('.tgz'):
+        archive = True
+        opener, mode = tarfile.open, 'r:gz'
+    elif path.endswith('.tar.bz2') or path.endswith('.tbz'):
+        archive = True
+        opener, mode = tarfile.open, 'r:bz2'
+    elif path.endswith('.gz'):
+        opener, mode = gzip.open, 'rb'
+        outfile, outmode = path[:-3], 'wb'
+    else:
+        raise ValueError(f"Could not extract {path} as no appropriate extractor is found")
+
+    with opener(filename, mode) as f_in:
+        if archive:
+            logger.info(f"Extracting {filename.name}")
+            f_in.extractall(path=dst_dir)
+        else:
+            outfile = pathlib.Path(outfile).name
+            logger.info(f"Decompresing {outfile}")
+            with open(pathlib.Path(dst_dir) / outfile, outmode) as f_out:
+                shutil.copyfileobj(f_in, f_out)
