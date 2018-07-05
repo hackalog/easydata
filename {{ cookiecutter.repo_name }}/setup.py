ca6e1b0d150d85046d3a3356beae7a2fe1edb2b0
Kjell Wooding <kjell@wooding.org>
2018-07-05 18:47:19 -0400

    rename the old src repo

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='{{ cookiecutter.repo_name }}',
     packages=find_packages(),
     version='0.0.1',
     description='{{ cookiecutter.description }}',
