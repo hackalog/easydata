5863cb1660a60ff2fb966f955e9f71f25de7ddba
Kjell Wooding <kjell@wooding.org>
2018-07-12 21:33:15 -0400

    Improve decompression. More helper functions for datasets

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/data/datasets.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/data/datasets.py
@@ -1,10 +1,15 @@
+import cv2
+import glob
 import logging
 import os
 import pathlib
+import pandas as pd
+import numpy as np
 import json
 from sklearn.datasets.base import Bunch
+from scipy.io import loadmat
 from functools import partial
-from joblib import Memory
+import joblib
 import sys
 
 from .utils import fetch_and_unpack, get_dataset_filename
@@ -14,19 +19,37 @@ _MODULE = sys.modules[__name__]
 _MODULE_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
 logger = logging.getLogger(__name__)
 
-jlmem = Memory(cachedir=str(interim_data_path))
-
 def new_dataset(*, dataset_name):
+    """Return an unpopulated dataset object.
+
+    Fills in LICENSE and DESCR if they are present.
+    Takes metadata from the url_list object if present. Otherwise, if
+    `*.license` or `*.readme` files are present in the module directory,
+    these will be as LICENSE and DESCR respectively.
+    """
     global dataset_raw_files
 
     dset = Bunch()
     dset['metadata'] = {}
     dset['LICENSE'] = None
     dset['DESCR'] = None
+    filemap = {
+        'LICENSE': f'{dataset_name}.license',
+        'DESCR': f'{dataset_name}.readme'
+    }
 
+    # read metadata from disk if present
+    for metadata_type in filemap:
+        metadata_file = _MODULE_DIR / filemap[metadata_type]
+        if metadata_file.exists():
+            with open(metadata_file, 'r') as fd:
+                dset[metadata_type] = fd.read()
+
+    # Use downloaded metadata if available
     ds = dataset_raw_files[dataset_name]
     for fetch_dict in ds.get('url_list', []):
         name = fetch_dict.get('name', None)
+        # if metadata is present in the URL list, use it
         if name in ['DESCR', 'LICENSE']:
             txtfile = get_dataset_filename(fetch_dict)
             with open(raw_data_path / txtfile, 'r') as fr:
@@ -47,27 +70,87 @@ def add_dataset_by_urllist(dataset_name, url_list):
     dataset_raw_files = read_datasets()
     return dataset_raw_files[dataset_name]
 
-@jlmem.cache
-def load_dataset(dataset_name, return_X_y=False, **kwargs):
+def add_dataset_metadata(dataset_name, from_file=None, from_str=None, kind='DESCR'):
+    """Add metadata to a dataset
+
+    from_file: create metadata entry from contents of this file
+    from_str: create metadata entry from this string
+    kind: {'DESCR', 'LICENSE'}
+    """
+    global dataset_raw_files
+
+    filename_map = {
+        'DESCR': f'{dataset_name}.readme',
+        'LICENSE': f'{dataset_name}.license',
+    }
+
+    if dataset_name not in dataset_raw_files:
+        raise Exception(f'No such dataset: {dataset_name}')
+
+    if kind not in filename_map:
+        raise Exception(f'Unknown kind: {kind}. Must be one of {filename_map.keys()}')
+
+    if from_file is not None:
+        with open(from_file, 'r') as fd:
+            meta_txt = fd.read()
+    elif from_str is not None:
+        meta_txt = from_str
+    else:
+        raise Exception(f'One of `from_file` or `from_str` is required')
+
+    with open(_MODULE_DIR / filename_map[kind], 'w') as fw:
+        fw.write(meta_txt)
+
+def load_dataset(dataset_name, return_X_y=False, force=False, **kwargs):
     '''Loads a scikit-learn style dataset
 
     dataset_name:
         Name of dataset to load
     return_X_y: boolean, default=False
         if True, returns (data, target) instead of a Bunch object
+    force: boolean
+        if True, do complete fetch/process cycle. If False, will use cached object (if present)
     '''
 
     if dataset_name not in dataset_raw_files:
         raise Exception(f'Unknown Dataset: {dataset_name}')
 
-    fetch_and_unpack(dataset_name)
-
-    dset = dataset_raw_files[dataset_name]['load_function'](**kwargs)
+    # check for cached version
+    cache_file = processed_data_path / f'{dataset_name}.jlib'
+    if cache_file.exists() and force is not True:
+        dset = joblib.load(cache_file)
+    else:
+        # no cache. Regenerate
+        fetch_and_unpack(dataset_name)
+        dset = dataset_raw_files[dataset_name]['load_function'](**kwargs)
+        with open(cache_file, 'wb') as fo:
+            joblib.dump(dset, fo)
 
     if return_X_y:
         return dset.data, dset.target
     else:
         return dset
+
+def read_space_delimited(filename, skiprows=None, class_labels=True):
+    """Read an space-delimited file
+
+    skiprows: list of rows to skip when reading the file.
+
+    Note: we can't use automatic comment detection, as
+    `#` characters are also used as data labels.
+    class_labels: boolean
+        if true, the last column is treated as the class label
+    """
+    with open(filename, 'r') as fd:
+        df = pd.read_table(fd, skiprows=skiprows, skip_blank_lines=True, comment=None, header=None, sep=' ', dtype=str)
+        # targets are last column. Data is everything else
+        if class_labels is True:
+            target = df.loc[:,df.columns[-1]].values
+            data = df.loc[:,df.columns[:-1]].values
+        else:
+            data = df.values
+            target = np.zeros(data.shape[0])
+        return data, target
 
 def write_dataset(path=None, filename="datasets.json", indent=4, sort_keys=True):
     """Write a serialized (JSON) dataset file"""
