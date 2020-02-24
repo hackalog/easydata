08c8e84a71fc0a73d0591e60cf92c5ea5da2e2ab
Kjell Wooding <kjell@wooding.org>
2020-02-24 15:19:31 -0500

    fixes #67

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -4,7 +4,7 @@ setup(
     name='{{ cookiecutter.module_name }}',
     packages=find_packages(),
     version='0.0.1',
-    description='{{ cookiecutter.description }}',
+    description='''{{ cookiecutter.description }}''',
     author='{{ cookiecutter.author_name }}',
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-2-Clause' %}BSD-2{% endif %}',
 )
