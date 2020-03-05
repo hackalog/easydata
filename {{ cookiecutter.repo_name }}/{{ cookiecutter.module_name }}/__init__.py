7edc8844bc1cc8c3b7d48f34d8be62e5c9c4b89b
Kjell Wooding <kjell@wooding.org>
2020-03-05 15:47:51 -0500

    fix location of catalog

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -1,6 +1,8 @@
 import pathlib
 from ._paths import Paths
 
+_module_dir = pathlib.Path(__file__).parent.resolve()
+
 _path_defaults = {
     'data_path': '${project_path}/data',
     'raw_data_path': '${data_path}/raw',
@@ -8,6 +10,6 @@ _path_defaults = {
     'processed_data_path': '${data_path}/processed',
     'project_path': '${catalog_path}/..',
 }
-_catalog_file = pathlib.Path("../catalog/config.ini")
+_catalog_file = _module_dir.parent / "catalog" / "config.ini"
 
 paths = Paths(_path_defaults, config_file=_catalog_file, config_section="Paths")
