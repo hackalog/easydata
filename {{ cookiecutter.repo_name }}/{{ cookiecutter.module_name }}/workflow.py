fb77008a626d36d2f7a5c1aa5eab27600b3e6886
Amy Wooding <amy@wooding.org>
2020-11-02 15:07:03 -0700

    add helper function

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/workflow.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/workflow.py
@@ -32,7 +32,7 @@ from .data import (cached_datasets, dataset_catalog,
                    datasource_catalog, add_datasource, load_catalog, del_from_catalog,
                    process_datasources as make_sources)
 from .data import (transformer_catalog, add_dataset, dataset_from_csv_manual_download,
-                   apply_transforms as make_data, dataset_from_metadata)
+                   apply_transforms as make_data, dataset_from_metadata, dataset_from_single_function)
 
 __all__ = [
     'add_dataset',
@@ -42,6 +42,7 @@ __all__ = [
     'datasource_catalog',
     'dataset_from_csv_manual_download',
     'dataset_from_metadata',
+    'dataset_from_single_function',
     'del_from_catalog',
     'load_catalog',
     'make_data',
